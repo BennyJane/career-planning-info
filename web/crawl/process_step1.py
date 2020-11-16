63d2ad2908ccd7e99c72d444db0de97663d7f200
Benny Jane <3355817143@qq.com>
2020-11-16 18:03:48 +0800

    更新

--- web/crawl/process_step1.py
+++ web/crawl/process_step1.py
@@ -7,14 +7,12 @@
 import json
 
 import pandas as pd
-
+from pprint import pprint
 from web.crawl.utils import clean_demand
 
 """
-功能：
-- 抓取数据的清理
-- 数据格式整理
-- 数据存储到数据库
+提供页面展示信息：
+- 优先去重复
 """
 
 
@@ -24,24 +22,60 @@ def strToJson(x):
     return json.dumps(demands, ensure_ascii=False)
 
 
-class ProcessData(object):
+class ProcessStep1(object):
     saved_path = './cleaned'
 
     def __init__(self, path="./job_20201108.csv"):
         self.path = path
         self.df = pd.read_csv(path)
+        self.dropDuplicated()
+
+    def dropDuplicated(self):
+        self.df = self.df.drop_duplicates(subset=['name', 'salary', 'tags', 'companyName', 'url'], keep='first')
 
     def core(self):
-        # print(self.df)
-        res = self.df['jobDemand'].unique().tolist()
-        # print(res)
-        all_demand = []
-        for item in res:
-            demands = json.loads(item)
-            all_demand.extend(demands)
-            # break
-        print(set(all_demand))
-        # print(res)
+        total_chart = self.totalChart()
+        year_chart = self.yearChart()
+        pprint(locals())
+        return dict(**locals())
+
+    def totalChart(self):
+        rowNum = self.df.shape[0]
+        # todo 原始数据需要处理,按照更新时间降序排列
+        df = self.df.groupby(by=['updateTime']).count()
+        df = df.iloc[-10:, :1]
+        last_update = []
+        for item in df.itertuples():
+            last_update.append({
+                "date": item[0],
+                "name": item[1]
+            })
+        totalStat = {
+            "total": rowNum,
+            "barData": last_update,
+        }
+        return totalStat
+
+    def yearChart(self):
+        # yearCategory = self.df['years'].unique().tolist()
+        yearCategory = ['3-5年', '5-10年', '1-3年', '1年以内']
+        result = {}
+
+        for year in yearCategory:
+            result[year] = []
+            df = self.df[self.df['years'] == year]
+            res = df.to_dict(orient='records')
+            # print(res)
+            for row in res:
+                result[year].append({
+                    "company": row['companyName'],
+                    "job": row['name'],
+                    "salary": row['salary'],
+                    "site": row['site'],
+                    "tags": row['tags'],
+                    "url": row['url'],
+                })
+        return result
 
     def correct(self):
         self.deal_job_demand()
@@ -72,7 +106,5 @@ class ProcessData(object):
 
 
 if __name__ == '__main__':
-    pro = ProcessData()
-    # pro.core()
-    pro.demands_to_csv()
-    # pro.correct()
+    pro = ProcessStep1()
+    pro.core()
