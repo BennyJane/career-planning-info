cbb8b0d16f023b5fb2aa05b6067d58cfe6b93540
BennyJane <3355817143@qq.com>
2020-12-27 00:48:36 +0800

    完成docker-compose直接部署

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,11 @@
 #set -ex
 set -e
 
-cd `dirname "$0"`
-currentPath=`pwd`
+cd $(dirname "$0")
+currentPath=$(pwd)
 
 echo "[当前目录]： $(pwd)"
 
-webLogPath=$currentPath + '/logs/web'
-redisLogPath=$currentPath + '/logs/redis'
-gunicornLogPath=$currentPath + '/logs/gunicorn'
-
-
-`flask db upgrade`
+$(flask db upgrade)
 sleep 4
-`gunicorn -c gunicorn.conf.py application:app --preload`
+$(gunicorn -c gunicorn.conf.py application:app --preload)
